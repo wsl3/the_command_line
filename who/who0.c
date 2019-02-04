0ccb02e758de9c008fe2a2e530f9eaae2179382a
wsl3 <2350622075@qq.com>
2019-02-04 13:11:52 +0800

    who 0.0 edition

---
+++ who/who0.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>//use exit()
+#include<utmp.h> //structure of login record
+#include<fcntl.h> // open()
+#include<unistd.h> // read() and close()
+
+void show_info(struct utmp*);
+
+int main(int argc, char * argv[])
+{
+	struct utmp current_utmp;
+	int fp;
+	int size = sizeof(current_utmp);
+	//open the file and get the descripter
+	if((fp = open(UTMP_FILE, O_RDONLY)) == -1)
+	{
+		perror(UTMP_FILE);
+		exit(1);
+	}
+	
+	//read if open is success
+	while(read(fp, &current_utmp, size) == size)
+	{
+		show_info(&current_utmp);
+	}	
+	return 0;
+}
+
+void show_info(struct utmp* ut)
+{
+	printf("username:%-10s", ut->ut_name);
+	printf("device:%-10s", ut->ut_line);
+	printf("host:%-10s\n", ut->ut_host);
+}
